d57cbc386ae20776a0f7feb9f54e160e6ae34750
KuroNgo <hoaiphong01012002@gmail.com>
2023-05-15 23:57:48 +0700

    Complete All Functions

---
+++ HotelManagement_Customer/HotelManagement_Customer.Data/Repository/ErrorRepository.cs
@@ -0,0 +1,21 @@
+﻿using HotelManagement_Customer.Data.Infrastructure;
+using HotelManagement_Customer.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement_Customer.Data.Repository
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+
+    }
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        protected ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ HotelManagement_Customer/HotelManagement_Customer.WebMVC/App_Start/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using HotelManagement_Customer.Data;
+using HotelManagement_Customer.Data.Infrastructure;
+using HotelManagement_Customer.Data.Repository;
+using HotelManagement_Customer.Service;
+using Microsoft.Owin;
+using Owin;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+
+[assembly: OwinStartup(typeof(TeduShop.Web.App_Start.Startup))]
+
+namespace TeduShop.Web.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //Register WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<HotelManagement_Customer_DbContext>().AsSelf().InstancePerRequest();
+
+            // Repositoies
+            builder.RegisterAssemblyTypes(typeof(UserAccountRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(UserServices).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container);
+        }
+    }
+}
