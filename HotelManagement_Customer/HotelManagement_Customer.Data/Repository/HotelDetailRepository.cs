5aab122b0b73618a422412a8994186fa39fa419a
Victor <quanggvuu1004@gmail.com>
2023-05-18 16:55:45 +0700

    update hoteldetail tests

--- HotelManagement_Customer/HotelManagement_Customer.Data/Repository/HotelDetailRepository.cs
+++ HotelManagement_Customer/HotelManagement_Customer.Data/Repository/HotelDetailRepository.cs
@@ -1,11 +1,16 @@
 ﻿using HotelManagement_Customer.Data.Infrastructure;
 using HotelManagement_Customer.Model.Model;
+using System.Collections.Generic;
 
 namespace HotelManagement_Customer.Data.Repository
 {
     public interface IHotelDetailRepository : IRepository<HotelDetail>
     {
-
+        HotelDetail GetHotelDetailById(int hotelId);
+        IEnumerable<HotelDetail> GetAllHotelDetails();
+        void AddHotelDetail(HotelDetail hotelDetail);
+        void UpdateHotelDetail(HotelDetail hotelDetail);
+        void DeleteHotelDetail(HotelDetail hotelDetail);
     }
     public class HotelDetailRepository : RepositoryBase<HotelDetail>, IHotelDetailRepository
     {
@@ -14,5 +19,31 @@ namespace HotelManagement_Customer.Data.Repository
         {
 
         }
+
+        public HotelDetail GetHotelDetailById(int hotelId)
+        {
+            return GetSingleById(hotelId);
+        }
+
+        public IEnumerable<HotelDetail> GetAllHotelDetails()
+        {
+            return GetAll();
+        }
+
+        public void AddHotelDetail(HotelDetail hotelDetail)
+        {
+            Add(hotelDetail);
+        }
+
+        public void UpdateHotelDetail(HotelDetail hotelDetail)
+        {
+            Update(hotelDetail);
+        }
+
+        public void DeleteHotelDetail(HotelDetail hotelDetail)
+        {
+            Delete(hotelDetail);
+        }
     }
 }
+
